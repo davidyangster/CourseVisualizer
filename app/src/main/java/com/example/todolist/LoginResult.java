acfc3aef82f8841f03d72ce4ed76512268f6c942
Patrick Cervantes <Patrick.Cervantes@ubc365.onmicrosoft.com>
2020-01-11 22:52:22 -0800

    Course list with pie chart

---
+++ app/src/main/java/com/example/todolist/LoginResult.java
@@ -0,0 +1,19 @@
+package com.example.todolist;
+
+import com.google.gson.annotations.SerializedName;
+
+public class LoginResult {
+
+    private String name;
+
+    private String username;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
